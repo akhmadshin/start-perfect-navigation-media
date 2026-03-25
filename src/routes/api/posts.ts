19153d88114c2f0e55c2c535dddd91199aa8050b
akhmadshin <qwasderfasder@gmail.com>
2026-03-25 15:32:07 +0700

    Fix listing api

--- src/routes/api/posts.ts
+++ src/routes/api/posts.ts
@@ -6,7 +6,16 @@ export const Route = createFileRoute('/api/posts')({
   server: {
     handlers: {
       GET: async () => {
-        const articles = Array.from(Array(20).keys()).map((id) => articlesList[id]);
+        // @ts-ignore
+        const articles = Array.from(Array(20).keys())
+          .map((id) => articlesList[id])
+          .map(({ attributes, id }) => {
+            const { content, ...formattedAttributes } = attributes;
+            return {
+              id,
+              attributes: formattedAttributes
+            }
+          });
         return json({
           data: articles,
           meta: {},
